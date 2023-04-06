cba9d01dd28b112c73b3eef6cc3012d8a287d15b
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-06 03:02:19 -0700

    changes

--- client/src/api/UserRequests.js
+++ client/src/api/UserRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5002" });
+const API = axios.create({ baseURL: "https://chat-mern-react.onrender.com" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
--- client/src/components/Coversation/Conversation.jsx
+++ client/src/components/Coversation/Conversation.jsx
@@ -24,7 +24,7 @@ const Conversation = ({ data, currentUser, online }) => {
     }
 
     getUserData();
-  }, [])
+  }, [dispatch])
   return (
     <>
       <div className="follower conversation">
